16c1f58c237d00b2e830da717611cfe551e54862
tmba <ut42.nu@gmail.com>
2025-09-13 16:43:15 +0900

    テーマのデフォルト設定を「system」から「light」に変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         <SessionProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
